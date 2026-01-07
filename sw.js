3d850902413281af9f1100284d0099b1b782aafb
negapo999-game <negapo999@gmail.com>
2026-01-07 19:55:40 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,7 @@
-const CACHE_NAME = "word-app-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+self.addEventListener("install", () => {
+  console.log("Service Worker installed");
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", () => {
+  // 何もしない（←これが安定）
 });
-
